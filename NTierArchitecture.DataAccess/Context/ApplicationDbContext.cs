cbfa2e082bc71b2d8da03c6aa199324b1736ed39
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-04-13 14:57:21 +0300

    DataAccess Katmanı Ve Db Context Yapısı

---
+++ NTierArchitecture.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NTierArchitecture.Entities.Models;
+
+namespace NTierArchitecture.DataAccess.Context;
+internal sealed class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+
+}
